2ecf8dcd3617463ecaf7d1b1477f984e73fff928
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-04-17 10:04:36 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
+from .nodes import LoadImage, LoadKimiVLModel, KimiVL, SaveText
 
+NODE_CLASS_MAPPINGS = {
+    "LoadImage": LoadImage,
+    "LoadKimiVLModel": LoadKimiVLModel,
+    "KimiVL": KimiVL,
+    "SaveText": SaveText,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadImage": "Load Image",
+    "LoadKimiVLModel": "Load Kimi VL Model",
+    "KimiVL": "Kimi VL",
+    "SaveText": "Save Text",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
